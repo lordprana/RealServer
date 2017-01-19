44e4c9206d9544ddacc674175492dfc2c742a55a
matthewgaba <matthew.gaba.2@gmail.com>
2017-01-19 13:05:32 -0600

    Debugging Heroku

--- settings.py
+++ settings.py
@@ -150,15 +150,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-ENV_PATH = os.path.abspath(os.path.dirname(__file__))
-MEDIA_ROOT = os.path.join(ENV_PATH, 'mediafiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'mediafiles')
 MEDIA_URL = "/media/"
-STATIC_ROOT =  os.path.join(ENV_PATH, 'staticfiles')
+STATIC_ROOT =  os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
-    os.path.join(ENV_PATH, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 YELP_APP_ID = 's7-DcAMdseJJmTHuki81Wg'
