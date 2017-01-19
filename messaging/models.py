312cc1c42bb0d610b54173b7aca55a05e2d14381
matthewgaba <matthew.gaba.2@gmail.com>
2017-01-19 12:39:53 -0600

    Preparing push for heroku

--- messaging/models.py
+++ messaging/models.py
@@ -9,6 +9,7 @@ class Message(models.Model):
     index = models.IntegerField()
     content = models.CharField(max_length=480)
     time_sent = models.DateTimeField(auto_now_add=True)
+
     sent_by = models.ForeignKey('api.User', related_name='sent_by')
     sent_to = models.ForeignKey('api.User', related_name='sent_to')
     read = models.BooleanField(default=False)
