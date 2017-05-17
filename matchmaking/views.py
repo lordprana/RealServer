5b5377a752cb33271b04685ffe3a754b5afd1176
matthewgaba <matthew.gaba.2@gmail.com>
2017-05-16 22:15:34 -0500

    Fix bug that causes internal server error when non standard unicode character is present in place-id

--- matchmaking/views.py
+++ matchmaking/views.py
@@ -268,8 +268,8 @@ def makeDate(user, day, potential_matches):
                             user.save()
                             match.save()
                             # Log date impressions
-                            print('User {0}: date impression at {1}'.format(user.pk, date.place_id))
-                            print('User {0}: date impression at {1}'.format(match.pk, date.place_id))
+                            print('User {0}: date impression at {1}'.format(user.pk, date.place_id).decode('utf-8'))
+                            print('User {0}: date impression at {1}'.format(match.pk, date.place_id).decode('utf-8'))
 
                             # Get Mutual Friends
                             mutual_friends_json = facebook.getMutualFriends(user, potential_match)
